02f83fe43d9e372aac815d126fb5d96a6729819e
Rhaiti <rhaiti.younes@gmail.com>
2013-06-08 19:08:14 +0200

    Create ajaxload.js
    la fonction load de ajax

---
+++ ajaxload.js
@@ -0,0 +1,14 @@
+$(document).ready(function(e) {
+   
+  $('body').load('fichier.html #c', chargementok);
+	
+	
+
+	
+});
+
+	function chargementok(){
+		
+		console.log("chargement terminé");
+		
+	}
